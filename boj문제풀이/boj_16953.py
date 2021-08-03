5601b39f9dbc2ff2722c818d103bc7d25dbdbbec
dku19jam <70689205+dku19jam@users.noreply.github.com>
2021-08-04 04:33:49 +0900

    알고리즘

--- boj문제풀이/boj_16953.py
+++ boj문제풀이/boj_16953.py
@@ -4,13 +4,13 @@ cnt = 1
 while True:
     if a == b:
         break
-    elif b < a or b%2 != 0 and b % 10 != 1:
+    elif b < a or b % 2 != 0 and b % 10 != 1:
         cnt = -1
         break
     elif b % 10 == 1:
         b //= 10
         cnt += 1
-    elif b%2 == 0:
+    elif b % 2 == 0:
         b //= 2
         cnt += 1
 
