4fd99367ee3179754dd21dafe25a44d2e3a41fb9
dku19jam <70689205+dku19jam@users.noreply.github.com>
2021-08-16 04:02:12 +0900

    알고리즘

--- boj문제풀이/boj_1912.py
+++ boj문제풀이/boj_1912.py
@@ -3,6 +3,6 @@ list = list(map(int,input().split()))
 
 sum = [list[0]]
 for i in range(len(list)-1):
-    sum.append(max(sum[i]+list[i+1],list[i+1]))
+    sum.append(max(sum[i]+list[i+1], list[i+1]))
 
 print(max(sum))
