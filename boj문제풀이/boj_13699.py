6fd52dcba20a1f1bf91d9d0ef828c6ac80e558bb
dku19jam <70689205+dku19jam@users.noreply.github.com>
2021-08-18 16:25:20 +0900

    Update boj_13699.py

--- boj문제풀이/boj_13699.py
+++ boj문제풀이/boj_13699.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 n = int(input())
 dp = [0] * (36)
 dp[0] = 1
@@ -14,21 +14,3 @@ for i in range(3, n+1):
         for j in range(i//2):
             t += 2*dp[j]*dp[i-j-1]
         dp[i] = t
-=======
-n = int(input()) 
-dp = [0] * (36)
-dp[0] = 1
-dp[1] = 1
-dp[2] = 2
-for i in range(3, n+1):
-    t = 0
-    if i%2:
-        for j in range(i//2):
-            t += 2*dp[j]*dp[i-j-1]
-        dp[i] = t + dp[i//2]**2
-    else:
-        for j in range(i//2):
-            t += 2*dp[j]*dp[i-j-1]
-        dp[i] = t
->>>>>>> cf13615b2933c8e61182ea4bc444c659e0c79e39
-print(dp[n])
